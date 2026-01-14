322db6458afb129127b1fdcd06661ab88d10d1a4
Matheus Alves <matheussilvaemac@gmail.com>
2026-01-14 13:54:06 -0300

    started new feature

--- src/components/InfiniteSkills.jsx
+++ src/components/InfiniteSkills.jsx
@@ -1,12 +1,25 @@
 import { skills } from '../constants/index'
+import { useState } from 'react'
 
 const InfiniteSkills = () => {
+
+  const [aboutSkill, setAboutSkill] = useState(true)
+
+  const handleCard = () => {
+    setAboutSkill(!aboutSkill)
+  }
+
+  const toggleAnimate = aboutSkill ? "" : "animation-paused"
+
   return (
     <div className="relative w-full h-auto mb-2 rounded-md">
-      <div className="animate-slide-two flex gap-10 w-300 lg:min-w-max">
-          <div className="flex gap-10 items-center">
+      <div className={`animate-slide-two ${toggleAnimate} flex gap-10 w-300 lg:min-w-max`}>
+          <div className="flex gap-10 items-center ">
             {skills.map((logo, index) => (
-            <div key={index}>
+            <div
+             onClick={handleCard}
+             className='bg-gradient-to-b from-indigo-700/20 to-indigo-700/20 via-transparent border-2 border-indigo-800  h-90 w-70 flex justify-center items-center rounded-4xl'
+             key={index}>
              <img 
                 src={logo.imgPath} 
                 alt={`logo ${logo.name}`} 
